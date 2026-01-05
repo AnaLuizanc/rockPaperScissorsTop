9ce764af007eaa6d2237eda6179309dc045e25f9
Ana Luiza <analuizanc13@gmail.com>
2026-01-05 16:16:07 +0000

    feat: add initial HTML and JavaScript files for Rock Paper Scissors game

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"]
+    let index = Math.floor(Math.random() * 2)
+
+    console.log(choices)
+    console.log(index)
+
+}
