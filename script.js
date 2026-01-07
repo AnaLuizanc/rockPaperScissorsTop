5aa2b5e264a3687ec33334fd72aa9a60da624243
AnaLuizanc <analuizanc13@gmail.com>
2026-01-06 22:16:51 -0300

    feat: implement game round logic and update score tracking for Rock Paper Scissors

--- script.js
+++ script.js
@@ -25,4 +25,26 @@ function getHumanChoice() {
 
 function handleHumanChoice(choice) {
     console.log("Human chose:", choice);
+    return choice
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    console.warn(humanChoice)
+    console.warn(computerChoice)
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+        humanScore = humanScore + 1;
+        computerScore = computerScore + 1
+    } else if (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win!")
+        humanScore = humanScore + 1;
+    } else {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice)
+        computerScore = computerScore + 1
+    }
+}
+
+
