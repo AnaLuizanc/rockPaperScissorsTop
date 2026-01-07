152cbbff895cb1ea438bc99b0b288634678cc232
AnaLuizanc <analuizanc13@gmail.com>
2026-01-06 21:36:05 -0300

    feat: implement game logic and update HTML structure for Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"]
-    let index = Math.floor(Math.random() * 2)
+    let index = Math.floor(Math.random() * 3)
 
-    console.log(choices)
-    console.log(index)
+    return choices[index]
+}
 
+function getHumanChoice() {
+    let userChoice = ""
+
+    document.getElementById("rock").onclick = function () {
+        userChoice = "rock"
+    }
+
+    document.getElementById("paper").onclick = function () {
+        userChoice = "paper"
+    }
+
+    document.getElementById("scissors").onclick = function () {
+        userChoice = "scissors"
+    }
+
+    return userChoice
+}
+
+function handleHumanChoice(choice) {
+    console.log("Human chose:", choice);
 }
