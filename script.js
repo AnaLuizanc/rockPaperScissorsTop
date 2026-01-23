dedc1fd5443e02fdb493d09462f96965325afbf7
AnaLuizanc <analuizanc13@gmail.com>
2026-01-23 11:36:18 -0300

    feat: add divs for better page structure and add styling
    - adds styling to page components;
    - adds SVG images

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+import { userProfileIcon } from "./svgIcons.js";
+import {robotIcon} from "./svgIcons.js";
+import { versusIcon} from "./svgIcons.js";
+import {swordsIcon} from "./svgIcons.js";
+
+document.getElementById("userIcon").innerHTML = userProfileIcon;
+document.getElementById("robotIcon").innerHTML = robotIcon;
+document.getElementById("versusIcon").innerHTML = versusIcon;
+document.getElementById("swordsIcon").innerHTML = swordsIcon;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     const index = Math.floor(Math.random() * 3)
