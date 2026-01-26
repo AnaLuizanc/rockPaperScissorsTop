0db54eb2b2289962de1796a12d220a94fdc8a052
AnaLuizanc <analuizanc13@gmail.com>
2026-01-26 08:13:30 -0300

    feat: add mouse over event on buttons choice

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 import { userProfileIcon } from "./svgIcons.js";
-import {robotIcon} from "./svgIcons.js";
-import { versusIcon} from "./svgIcons.js";
-import {swordsIcon} from "./svgIcons.js";
+import { robotIcon } from "./svgIcons.js";
+import { versusIcon } from "./svgIcons.js";
+import { swordsIcon } from "./svgIcons.js";
 
 document.getElementById("userIcon").innerHTML = userProfileIcon;
 document.getElementById("robotIcon").innerHTML = robotIcon;
 document.getElementById("versusIcon").innerHTML = versusIcon;
 document.getElementById("swordsIcon").innerHTML = swordsIcon;
+
+document.querySelectorAll(".buttons button").forEach(button => {
+    button.addEventListener("mouseenter", () => {
+        console.log("Hovering:", button.id);
+    });
+});
