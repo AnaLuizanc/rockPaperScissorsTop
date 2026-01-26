c3a615317911f8de738fea7de48986846d5d66de
AnaLuizanc <analuizanc13@gmail.com>
2026-01-26 11:42:11 -0300

    refactor: improve separation of responsibilities for each file
    - Separates game logic from user interaction

--- gameLogic.js
+++ gameLogic.js
@@ -1,47 +1,22 @@
 
-function getComputerChoice() {
+export function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     const index = Math.floor(Math.random() * 3)
 
     return choices[index]
 }
 
-function getHumanChoice() {
-    return new Promise((resolve) => {
-        document.getElementById("rock").onclick = () => resolve("rock");
-        document.getElementById("paper").onclick = () => resolve("paper");
-        document.getElementById("scissors").onclick = () => resolve("scissors");
-    });
-}
-
-function updateScores(plusHuman = false, plusComputer = false) {
-    if (plusHuman) {
-        humanScore += 1;
-        document.getElementById("humanScore").innerHTML = humanScore;
-    }
-    if (plusComputer) {
-        computerScore += 1;
-        document.getElementById("computerScore").innerHTML = computerScore;
-    }
-}
-
-async function playRound(humanScore, computerScore) {
-    const humanChoice = await getHumanChoice();
-    const computerChoice = getComputerChoice();
-
-    console.warn("Computer's choice: " + computerChoice)
-    if (humanChoice === computerChoice) {
-        updateScores(true, true);
-        playRound(humanScore, computerScore);
+export function getWinner(human, computer) {
+    if (human === computer){
+        return "draw";
     } 
-    else if (humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "scissors" && computerChoice === "paper") {
-        updateScores(true, false);
-        playRound(humanScore, computerScore);
-    } 
-    else {
-        updateScores(false, true);
-        playRound(humanScore, computerScore);
+    if (
+        (human === "rock" && computer === "scissors") ||
+        (human === "paper" && computer === "rock") ||
+        (human === "scissors" && computer === "paper")
+    ) {
+        return "human";
     }
+
+    return "computer";
 }
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { icons } from "./svgIcons.js";
+import { getComputerChoice, getWinner } from "./gameLogic.js";
 
 const userPlaceholder = document.getElementById("userChoicePlaceholder");
 const robotPlaceholder = document.getElementById("robotChoicePlaceholder");
@@ -8,14 +9,47 @@ document.getElementById("robotIcon").innerHTML = icons.robot;
 document.getElementById("versusIcon").innerHTML = icons.versus;
 document.getElementById("swordsIcon").innerHTML = icons.swords;
 
+let roundActive = true;
+
 document.querySelectorAll(".buttons button").forEach(button => {
-    button.addEventListener("mouseenter", () => {
-        if (button.id === "rock") {
-            userPlaceholder.innerHTML = icons.rock;
-        } else if (button.id === "paper") {
-            userPlaceholder.innerHTML = icons.paper;
-        } else {
-            userPlaceholder.innerHTML = icons.scissors;
-        }
-    });
+
+  button.addEventListener("mouseenter", () => {
+    if (!roundActive) return;
+    userPlaceholder.innerHTML = icons[button.id];
+  });
+
+  button.addEventListener("click", () => {
+
+    if (!roundActive) return;
+    roundActive = false;
+
+    const humanChoice = button.id;
+    const computerChoice = getComputerChoice();
+
+    userPlaceholder.innerHTML = icons[humanChoice];
+    robotPlaceholder.innerHTML = icons[computerChoice];
+
+    const winner = getWinner(humanChoice, computerChoice);
+    updateScores(winner);
+
+    setTimeout(startNewRound, 3000);
+  });
+
 });
+
+function startNewRound() {
+  userPlaceholder.innerHTML = "";
+  robotPlaceholder.innerHTML = "";
+  roundActive = true;
+}
+
+function updateScores(winner) {
+    if (winner === "human") {
+        humanScore += 1;
+        document.getElementById("humanScore").innerHTML = humanScore;
+    }
+    if (winner === "computer") {
+        computerScore += 1;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    }
+}
