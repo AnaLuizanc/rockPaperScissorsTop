2706406b5a465194cf67f6d02b840fdd135e1aab
AnaLuizanc <analuizanc13@gmail.com>
2026-01-26 08:56:48 -0300

    feat: add dynamic image change according to the mouse pointer

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ import { userProfileIcon } from "./svgIcons.js";
 import { robotIcon } from "./svgIcons.js";
 import { versusIcon } from "./svgIcons.js";
 import { swordsIcon } from "./svgIcons.js";
+import { rockIcon } from "./svgIcons.js";
+import { paperIcon } from "./svgIcons.js";
+import { scissorsIcon } from "./svgIcons.js";
+
+const userPlaceholder = document.getElementById("userChoicePlaceholder");
 
 document.getElementById("userIcon").innerHTML = userProfileIcon;
 document.getElementById("robotIcon").innerHTML = robotIcon;
@@ -10,6 +15,15 @@ document.getElementById("swordsIcon").innerHTML = swordsIcon;
 
 document.querySelectorAll(".buttons button").forEach(button => {
     button.addEventListener("mouseenter", () => {
-        console.log("Hovering:", button.id);
+        if (button.id === "rock") {
+            userPlaceholder.innerHTML = rockIcon;
+        } else if (button.id === "paper") {
+            userPlaceholder.innerHTML = paperIcon;
+        } else {
+            userPlaceholder.innerHTML = scissorsIcon;
+        }
+    });
+    button.addEventListener("mouseleave", () => {
+        userPlaceholder.innerHTML = "";
     });
 });
